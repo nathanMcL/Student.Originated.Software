ce7b83c98f7889a0ad51c7f78fe074ec9395ab4c
Nathaniel Mclaughlin <Nmclaughlin296@gmail.com>
2024-04-17 23:53:29 +0000

    resolving error

--- Ai_SelfHosting/prototype-08/network.py
+++ Ai_SelfHosting/prototype-08/network.py
@@ -20,78 +20,78 @@ class Network(object):
     
     def SGD(self,
         training_data,
-        epochs,
-        mini_batch_size,
+        epochs, 
+        mini_batch_size, 
         eta,
-        test_data=None
-    ) :
+        test_data=None):
         if test_data:
             test_data = list(test_data)
             n_test = len(test_data)
-        n = len(list(training_data))
+        training_data = list(training_data)
+        n = len(training_data)
 
         for j in range(epochs):
-            training_data = list(training_data)
             random.shuffle(training_data)
             mini_batches = [
-                training_data[k:k+mini_batch_size]
-                for k in xrange(0, n, mini_batch_size)
+                training_data[k:k + mini_batch_size] for k in range(0, n, mini_batch_size)
             ]
 
             for mini_batch in mini_batches:
-                self.update_mini_batch(mini_batches, eta)
+                if mini_batch:  # Ensure mini_batch is not empty
+                    self.update_mini_batch(mini_batch, eta)
                 if test_data:
                     print("Epoch {0}: {1} / {2}".format(
                         j, self.evaluate(test_data), n_test))
                 else:
                     print("Epoch {0} complete".format(j))
 
-def update_mini_batch(self, mini_batch, eta):
-    nabla_b = [np.zeros(b.shape) for b in self.biases]
-    nabla_w = [np.zeros(w.shape) for w in self.weights]
-    for x, y in mini_batch:
-        delta_nabla_b, delta_nabla_w = self.backprop(x, y)
-        nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
-        nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
-    self.weights = [w-(eta/len(mini_batch))*nw
-                    for w, nw in zip(self.weights, nabla_w)]
-    self.biases = [b-(eta/len(mini_batch))*nb
-                    for b, nb in zip(self.biases, nabla_b)]
 
-def backprop(self, x, y):
-    nabla_b = [np.zeros(b.shape) for b in self.biases]
-    nabla_w = [np.zeros(w.shape) for w in self.weights]
-    # feed forward
-    activation = x
-    activations = [x] # list to store all the activations, layer by layer
-    zs = [] # list to store all the z vectors, layer by layer
-    for b, w in zip(self.biases, self.weights):
-        z = np.dot(w, activation)+b
-        zs.append(z)
-        activation = sigmoid(z)
-        activations.append(activation)
-    # backward pass
-    delta = self.cost_derivative(activations[-1], y) * \
-        sigmoid_prime(zs[-1])
-    nabla_b[-1] = delta
-    nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+    def update_mini_batch(self, mini_batch, eta):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        for x, y in mini_batch:
+            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        self.weights = [w-(eta/len(mini_batch))*nw
+                        for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b-(eta/len(mini_batch))*nb
+                        for b, nb in zip(self.biases, nabla_b)]
 
-    for l in xrange(2, self.num_layers):
-        z = zs[-l]
-        sp = sigmoid_prime(z)
-        delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
-        nabla_b[-l] = delta
-        nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
-    return (nabla_b, nabla_w)
+    def backprop(self, x, y):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        # feed forward
+        activation = x
+        activations = [x] # list to store all the activations, layer by layer
+        zs = [] # list to store all the z vectors, layer by layer
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation)+b
+            zs.append(z)
+            activation = sigmoid(z)
+            activations.append(activation)
+        # backward pass
+        delta = self.cost_derivative(activations[-1], y) * \
+            sigmoid_prime(zs[-1])
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
 
-def evaluate(self, test_data):
-    test_results = [(np.argmax(self.feedforward(x)), y)
-                        for (x, y) in test_data]
-    return sum(int(x == y) for (x, y) in test_results)
+        for l in xrange(2, self.num_layers):
+            z = zs[-l]
+            sp = sigmoid_prime(z)
+            delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
+            nabla_b[-l] = delta
+            nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
+        return (nabla_b, nabla_w)
 
-def cost_derivative(self, output_activations, y):
-    r"""Return the vector of partial derivatives \partial C_x /
-    \partial a for the output activations."""
-    return (output_activations-y)
+    def evaluate(self, test_data):
+        test_results = [(np.argmax(self.feedforward(x)), y)
+                            for (x, y) in test_data]
+        return sum(int(x == y) for (x, y) in test_results)
+
+    def cost_derivative(self, output_activations, y):
+        r"""Return the vector of partial derivatives \partial C_x /
+        \partial a for the output activations."""
+        return (output_activations-y)
 
 
