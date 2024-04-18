bb8899f4e432df33f4864968b6e5403d43fc57e8
Nathaniel Mclaughlin <Nmclaughlin296@gmail.com>
2024-04-18 17:40:38 +0000

    resolving errors

--- Ai_SelfHosting/prototype-08/network.py
+++ Ai_SelfHosting/prototype-08/network.py
@@ -79,7 +79,7 @@ class Network(object):
             activations.append(activation)
         # backward pass
         delta = self.cost_derivative(activations[-1], y) * \
-            sigmoid_prime(zs[-1])
+            self.sigmoid_prime(zs[-1])
         nabla_b[-1] = delta
         nabla_w[-1] = np.dot(delta, activations[-2].transpose())
 
