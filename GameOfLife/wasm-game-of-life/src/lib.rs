619f3c00b6d491a32787e80d79a29ebe8970c303
Nathaniel Mclaughlin <Nmclaughlin296@gmail.com>
2024-05-20 21:55:58 +0000

    Conway's game of life

---
+++ GameOfLife/wasm-game-of-life/src/lib.rs
@@ -0,0 +1,13 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello, Welcome to NMac's wasm-game-of-life!");
+}
