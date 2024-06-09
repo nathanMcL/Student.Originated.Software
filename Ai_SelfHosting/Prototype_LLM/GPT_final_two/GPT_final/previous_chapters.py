db742776606cf523204dca93463609bba676a598
Nathaniel Mclaughlin <Nmclaughlin296@gmail.com>
2024-06-09 11:25:37 +0000

    comment updates

--- Ai_SelfHosting/Prototype_LLM/GPT_final_two/GPT_final/previous_chapters.py
+++ Ai_SelfHosting/Prototype_LLM/GPT_final_two/GPT_final/previous_chapters.py
@@ -41,7 +41,7 @@ class GPTDatasetV1(Dataset):
 
 
 def create_dataloader_v1(txt, batch_size=4, max_length=256,
-                         stride=128, shuffle=True, drop_last=True, num_workers=4):
+                         stride=128, shuffle=True, drop_last=True, num_workers=4): # Number of workers relates to number of threads
     """
     Create a DataLoader for the GPT dataset.
 
@@ -59,7 +59,7 @@ def create_dataloader_v1(txt, batch_size=4, max_length=256,
     """
     tokenizer = tiktoken.get_encoding("gpt2")
     dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last, num_workers=num_workers)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last, num_workers=num_workers)  # Number of workers relates to number of threads
     return dataloader
 
 
